5e6ab1d9ae0f7ecd7f1f70ad39e5b6567eb139aa
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-02-14 17:45:57 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/my/first/packadge/MyFirstProgram.java
+++ sandbox/src/main/java/my/first/packadge/MyFirstProgram.java
@@ -4,7 +4,27 @@ public class MyFirstProgram {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello, world!");
+		hello("Anastasiya");
+		hello("user");
+		hello("Sweden");
+
+		double n = 5;
+		System.out.println("Площадь квадрата со стороной " + n + " = " + area(n));
+
+		double a = 9;
+		double b = 3;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
 	}
 
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area(double len) {
+		return len * len;
+	}
+
+	public static double area(double a, double b) {
+		return a * b;
+	}
 }
