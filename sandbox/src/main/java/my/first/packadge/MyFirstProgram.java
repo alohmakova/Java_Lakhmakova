5cec8efea9019b49cbbfe42e3c1ab3e498b21414
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-02-17 00:18:50 +0100

    4 пункт: Вычисление расстояния между двумя точками при помощи метода в классе Point
    Не уверена, что это правильно, но оно работает. Сначала пыталась перенести функцию distance в класс Point и убрать там ключевое слово static, но так выдаёт ошибку java: non-static method distance(my.first.packadge.Point,my.first.packadge.Point) cannot be referenced from a static context - на данный момент я не понимаю, что означает этот " static context", поэтому до меня не доходит, почему в лекции, когда речь шла о методах вычисления квадрата и прямоугольника, они были без слова static, а теперь уже нужен static в методе distance.

--- sandbox/src/main/java/my/first/packadge/MyFirstProgram.java
+++ sandbox/src/main/java/my/first/packadge/MyFirstProgram.java
@@ -17,7 +17,7 @@ public class MyFirstProgram {
 
 		Point p1 = new Point(7,12);
 		Point p2 = new Point(18,25);
-		System.out.println("Расстояние между точками А(" + p1.x + "," + p1.y + ") и В(" + p2.x + "," + p2.y + ") = " + distance(p1, p2));
+		System.out.println("Расстояние между точками А(" + p1.x + "," + p1.y + ") и В(" + p2.x + "," + p2.y + ") = " + Point.distance(p1, p2));
 
 
 	}
@@ -25,10 +25,5 @@ public class MyFirstProgram {
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
 	}
-
-	public static double distance(Point p1, Point p2) {
-		return Math.sqrt(((p2.x - p1.x)*(p2.x - p1.x)) + ((p2.y - p1.y)*(p2.y - p1.y)));
-	}
-
 
 }
--- sandbox/src/main/java/my/first/packadge/Point.java
+++ sandbox/src/main/java/my/first/packadge/Point.java
@@ -8,4 +8,9 @@ public class Point {
     public Point(double x, double y) {
         this.x=x; this.y=y;
     }
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(((p2.x - p1.x)*(p2.x - p1.x)) + ((p2.y - p1.y)*(p2.y - p1.y)));
+    }
+
 }
