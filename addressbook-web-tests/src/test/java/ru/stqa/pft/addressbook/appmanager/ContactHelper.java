52cd5c6749c7b059f688e3efc45aa2c36f999e12
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-02-24 12:58:03 +0100

    Теперь удаляю контакт по name=selected[], а не по id

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -29,8 +29,8 @@ public class ContactHelper extends HelperBase {
         click(By.xpath("//input[22]"));
     }
 
-    public void selectContact(String id) {
-        click(By.id(id));
+    public void selectContact() {
+        click(By.name("selected[]"));
     }
 
     public void DeleteContact() {
