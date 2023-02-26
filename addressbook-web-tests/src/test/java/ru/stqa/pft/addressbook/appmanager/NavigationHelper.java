52bba9326d760a58783154b3d787474c24f88374
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-02-26 01:05:33 +0100

    Оптимизация перехода на страницу AddPage

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -19,6 +19,10 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void goToAddPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")) {
+            return;
+        }
         click(By.linkText("add new"));
     }
 
