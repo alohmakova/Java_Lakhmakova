d74434432d56ad733de539eabe8cb98b3aa1e488
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-03-01 21:31:31 +0100

    Оптимизация класса ContactDeletionViaEditTests
    теперь ищем конкретную группу для заполнения формы контакта, а не любую

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionViaEditTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionViaEditTests.java
@@ -8,12 +8,11 @@ public class ContactDeletionViaEditTests extends TestBase{
     @Test
     public void testContactDeletion() throws Exception {
         if (! app.getContactHelper().isThereAContactToModify()) {
-            app.getNavigationHelper().goToAddPage();
+            app.getNavigationHelper().gotoGroupPage();
             if (! app.getGroupHelper().isThereAParticularGroup ()) {
-                app.getNavigationHelper().gotoGroupPage();
                 app.getGroupHelper().createGroupToAddNewContact(new GroupData("test", null, null));
-                app.getNavigationHelper().goToAddPage();
             }
+            app.getNavigationHelper().goToAddPage();
             app.getContactHelper().fillAndSubmitContactForm(new ContactData ("Теста", "Тестова", "Самара", "+79057590236", "ivanova@gmail.com", "test"));
             app.getNavigationHelper().returnToHomePage();
         }
