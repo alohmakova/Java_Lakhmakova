e0f55b68c777598dee3eec887939546fdf434e14
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-03-01 10:38:00 +0100

    Доработка тестов в классе PointTests

--- sandbox/src/main/java/my/first/packadge/MyFirstProgram.java
+++ sandbox/src/main/java/my/first/packadge/MyFirstProgram.java
@@ -15,8 +15,8 @@ public class MyFirstProgram {
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
 
-		Point p1 = new Point(5,5);
-		Point p2 = new Point(0,5);
+		Point p1 = new Point(-5,3);
+		Point p2 = new Point(0,9);
 		System.out.println("Расстояние между точками А(" + p1.x + "," + p1.y + ") и В(" + p2.x + "," + p2.y + ") = " + p1.distance(p2));
 
 		Equation e = new Equation(1,2,1);
--- sandbox/src/test/java/my/first/packadge/PointTests.java
+++ sandbox/src/test/java/my/first/packadge/PointTests.java
@@ -4,15 +4,18 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PointTests {
-    //в данном тесте в ожидаемом результате указан тип данных, который не соответствует актуальному результату
+    //проверяю, что результат должен быть одинаковый, независимо от того, положительные или отрицательные значения вводятся:
+    // точки перемещаются в другую часть системы координат, но расстояние от этого между ними не меняется
     @Test
     public void testPoint1() {
 
-        Point p1 = new Point(5,5);
-        Point p2 = new Point(5,0);
-        Assert.assertEquals(p1.distance(p2), 5);
+        Point p1 = new Point(8,15);
+        Point p2 = new Point(4,9);
+        Point p3 = new Point(-8,-15);
+        Point p4 = new Point(-4,-9);
+        Assert.assertEquals(p1.distance(p2), p3.distance(p4));
     }
-    //тест должен пройти, так как ожидаемый результат соответствует актуальному
+    //проверка, что расчёты программой производятся правильно
     @Test
     public void testPoint2() {
 
@@ -20,13 +23,13 @@ public class PointTests {
         Point p2 = new Point(5,0);
         Assert.assertEquals(p1.distance(p2), 5.0);
     }
-    //в данном тесте указан неверный второй параметр для функции distance, поэтому тест должен провалиться
+    //в данном тесте проверяем, что расстояние от точки до самой себя = 0
     @Test
     public void testPoint3() {
 
-        Point p1 = new Point(5,5);
-        Point p2 = new Point(5,0);
-        Assert.assertEquals(p1.distance(p1), 5.0);
+        Point p1 = new Point(3,3);
+        Point p2 = new Point(3,3);
+        Assert.assertEquals(p1.distance(p2), 0.0);
     }
 
 }
