19af69e7ba45d94c0ea1bf55390d853d12a56beb
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-03-04 02:13:58 +0100

    Задание 8 исправила, коммитила частями, вот конечный результат
    Сейчас я ищу группу с крнкретным именем, все предусловия отрабатывают

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -8,7 +8,7 @@ public class GroupDeletionTests extends TestBase {
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     if (! app.getGroupHelper().isThereAGroup()) {
-      app.getGroupHelper().createGroup(new GroupData("test", null, null));
+      app.getGroupHelper().createGroup(new GroupData("my_group", null, null));
     }
     app.getGroupHelper().selectGroup().click();
     app.getGroupHelper().DeleteSelectedGroups().click();
