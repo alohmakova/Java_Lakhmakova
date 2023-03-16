54fd46c3db0454879e90e9f8891b93b5e2f38a94
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-03-16 20:41:37 +0100

    Продолжаю редактировать метод all()
    Заменила строки с адресом и емейлом

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -151,9 +151,9 @@ public class ContactHelper extends HelperBase {
             int id = Integer.parseInt(cells.get(0).findElement(By.tagName ("input")).getAttribute ("value"));
             String lastName = cells.get(1).getText();
             String firstName = cells.get (2).getText();
-            String address = row.findElement(By.xpath("./td[4]")).getText();
+            String address = cells.get (3).getText();
             String telMobile = row.findElement(By.xpath("./td[6]")).getText();
-            String email = row.findElement(By.xpath("./td[5]")).getText();
+            String email = cells.get (4).getText();
             //int id = Integer.parseInt (element.findElement(By.tagName ("input")).getAttribute ("value"));
             contactCache.add (new ContactData ().withId (id).withFirstName (firstName).withLastName (lastName).withAddress (address).withTelMobile (telMobile).withEmail (email));
         }
