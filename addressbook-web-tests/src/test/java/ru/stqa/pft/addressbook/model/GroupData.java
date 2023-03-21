1e51e8bd6aa9aa0b41860df6d94e63e4c98d4451
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-03-21 11:07:30 +0100

    Генерация данных в формате XML

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,10 +1,13 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import java.util.Objects;
-
+@XStreamAlias ("group")
 public class GroupData {
-
-    private int id; //= Integer.MAX_VALUE;
+@XStreamOmitField()
+    private int id = Integer.MAX_VALUE;
     private String name;
     private String header;
     private String footer;
