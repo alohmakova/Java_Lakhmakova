fb2a3cb2334b3f9eb53206e6fe386eac7432a21f
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-03-29 16:48:21 +0200

    Условия отбора данных для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,38 +3,65 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
     @XStreamOmitField()
+    @Id
+    @Column (name = "id")
     private int id; //= Integer.MAX_VALUE;
     @Expose
+    @Column (name = "firstname")
     private String firstName;
     @Expose
+    @Column (name = "lastname")
     private String lastName;
     @Expose
+    @Column (name = "address")
+    @Type(type = "text")
     private String address;
     @Expose
+    @Column (name = "mobile")
+    @Type(type = "text")
     private String telMobile;
     @Expose
+    @Column (name = "home")
+    @Type (type = "text")
     private String telHome;
     @Expose
+    @Column (name = "work")
+    @Type (type = "text")
     private String telWork;
     @Expose
+    @Transient
     private String allPhones;
     @Expose
+    @Transient
     private String allEmails;
     @Expose
+    @Column (name = "email")
+    @Type(type = "text")
     private String email;
     @Expose
+    @Column (name = "email2")
+    @Type(type = "text")
     private String email2;
     @Expose
+    @Column (name = "email3")
+    @Type(type = "text")
     private String email3;
     @Expose
-    private File photo;
+    @Column (name = "photo")
+    @Type(type = "text")
+    private String photo;
     @Expose
+    @Transient
     private String group;
 
     public ContactData withId(int id) {
@@ -97,7 +124,7 @@ public class ContactData {
         return this;
     }
     public ContactData withPhoto(File photo) {
-        this.photo = photo;
+        this.photo = photo.getPath ();
         return this;
     }
 
@@ -154,7 +181,7 @@ public class ContactData {
         return email3;
     }
     public File getPhoto() {
-        return photo;
+        return new File (photo);
     }
 
     public String getGroup() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
@@ -7,7 +7,7 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.util.List;
 
@@ -32,16 +32,28 @@ public class HbConnectionTest {
         }
 
 
-        @Test
-        public void testHbConnection(){
-            try (Session session = sessionFactory.openSession ()) {
-                session.beginTransaction ();
-                List result = session.createQuery ("from GroupData").list ();
-                for (GroupData group : (List<GroupData>) result) {
-                    System.out.println (group);
-                }
-                session.getTransaction ().commit ();
-                session.close ();
+//        @Test(enabled = false)
+//        public void testHbConnection1(){
+//            try (Session session = sessionFactory.openSession ()) {
+//                session.beginTransaction ();
+//                List result = session.createQuery ("from GroupData").list ();
+//                for (GroupData group : (List<GroupData>) result) {
+//                    System.out.println (group);
+//                }
+//                session.getTransaction ().commit ();
+//                session.close ();
+//            }
+//        }
+    @Test
+    public void testHbConnection(){
+        try (Session session = sessionFactory.openSession ()) {
+            session.beginTransaction ();
+            List result = session.createQuery ("from ContactData where deprecated = '0000-00-00' ").list ();
+            for (ContactData contact : (List<ContactData>) result) {
+                System.out.println (contact);
             }
+            session.getTransaction ().commit ();
+            session.close ();
         }
+    }
 }
