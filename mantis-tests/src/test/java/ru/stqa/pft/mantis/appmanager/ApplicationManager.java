3cac98e33abf06fcbf55dfad7be1bea3424b93e9
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-04-01 19:51:05 +0200

    Добавлен новый модуль для тестов для Mantis

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,51 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+
+    private final Properties properties;
+    WebDriver wd;
+
+    private JavascriptExecutor js;
+    private String browser;
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+        properties = new Properties ();
+    }
+
+    public void init() throws IOException {
+        String target = System.getProperty ("target", "local");
+        properties.load (new FileReader (new File (String.format ("src/test/resources/%s.properties", target))));
+       if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+        }
+        wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+       //ожидание, чтобы проверить, если какие-то элементы на странице появляются позже, потому что долго загружаются
+        js = (JavascriptExecutor) wd;
+        wd.get(properties.getProperty ("web.baseUrl"));
+    }
+
+
+
+    public void stop() {
+        wd.findElement(By.linkText("Logout")).click();
+        wd.quit();
+    }
+}
