34b229eecacceaf2f3c8e45815b51b9bb5f2d7d5
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-04-02 19:02:05 +0200

    No tests found in the class ru.stqa.pft.mantis.tests.LoginTests

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/LoginTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/LoginTests.java
@@ -6,8 +6,9 @@ import ru.stqa.pft.mantis.appmanager.HttpSession;
 import java.io.IOException;
 
 import static org.testng.Assert.assertTrue;
+import static ru.stqa.pft.mantis.tests.TestBase.app;
 
-public class LoginTests extends TestBase {
+public class LoginTests {
     @Test
     public void testLogin() throws IOException {
         HttpSession session = app.newSession ();
