decbebe5fddd1c7ca0c4827b4f8d5f1df12fd9d0
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-04-02 18:48:50 +0200

    импортированы классы в HttpSession

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -1,6 +1,17 @@
 package ru.stqa.pft.mantis.appmanager;
 
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +39,7 @@ public class HttpSession {
         return body.contains (String.format ("<span class=\"italic\">%s<span>", username));
     }
 
-    private String eTextFrom(CloseableHttpResponse response) throws IOException {
+    private String geTextFrom(CloseableHttpResponse response) throws IOException {
         try {
             return EntityUtils.toString (response.getEntity ());
         } finally {
