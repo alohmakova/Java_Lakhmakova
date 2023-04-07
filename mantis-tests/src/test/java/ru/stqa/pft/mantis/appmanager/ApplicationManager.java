d7cd2e5821726a8ec1ad97f3930f1c981efe7438
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-04-07 23:58:24 +0200

    JamesHelper
    Получение почты на внешний почтовый сервер. Создание пользователей на внешнем почтовом сервере (по протоколу Telnet)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -24,6 +24,7 @@ public class ApplicationManager {
     private RegistrationHelper registrationHelper;
     private FtpHelper ftp;
     private MailHelper mailHelper;
+    private JamesHelper jamesHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -80,5 +81,11 @@ public class ApplicationManager {
             mailHelper = new MailHelper(this);
         }
         return mailHelper;
+    }
+    public JamesHelper james() {
+        if(jamesHelper == null){
+            jamesHelper = new JamesHelper(this);
+        }
+        return jamesHelper;
     }
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -13,20 +13,21 @@ import java.util.List;
 import static org.testng.AssertJUnit.assertTrue;
 
 public class RegistrationTests extends TestBase{
-    @BeforeMethod
+    //@BeforeMethod
     public void startMailServer(){
         app.mail ().start ();
     }
     @Test
     public void testRegistration() throws MessagingException, IOException {
         long now = System.currentTimeMillis ();
-        String email = String.format("user%s@localhost.localdomain", now);
+        String email = String.format("user%s@localhost", now);//username@localhost.localdomain
         String user = String.format("user%s", now);
         String password = "password";
+        app.james ().createUser (user, password);
         app.registration().start(user, email);
-        List<MailMessage> mailMessages = app.mail ().waitForMail (2, 10000);
+        //List<MailMessage> mailMessages = app.mail ().waitForMail (2, 10000);
+        List<MailMessage> mailMessages = app.james ().waitForMail (user, password, 60000);
         String confirmationLink = findConfirmationLink (mailMessages, email);
-
         app.registration().finish(confirmationLink, password);
        assertTrue(app.newSession ().login (user, password));
     }
@@ -37,11 +38,9 @@ public class RegistrationTests extends TestBase{
         return regex.getText (mailMessage.text);
     }
 
-    @AfterMethod(alwaysRun = true)
+    //@AfterMethod(alwaysRun = true)
     public void stopMailServer(){
         app.mail ().stop ();
-
-
     }
 }
 
