d227f35c5a372a9f0a6d195872b53e350f126294
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-04-15 21:53:49 +0200

    Тест исправлен и уменьшено количество тестов для групп в наборе для отладки

--- sandbox/src/test/java/my/first/packadge/SquareTests.java
+++ sandbox/src/test/java/my/first/packadge/SquareTests.java
@@ -8,7 +8,7 @@ public class SquareTests {
     public void testArea() {
 
         Square s = new Square(5);
-        Assert.assertEquals(s.area(), 20.0);
+        Assert.assertEquals(s.area(), 25.0);
 
     }
 }
