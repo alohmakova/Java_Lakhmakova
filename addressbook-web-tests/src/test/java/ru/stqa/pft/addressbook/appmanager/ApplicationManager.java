058c04062b97c1f81cee072d7f7dc18cbbe870da
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-04-15 23:57:51 +0200

    Реализована возможность удалённого запуска браузера

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,16 +1,20 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -36,12 +40,19 @@ public class ApplicationManager {
         String target = System.getProperty ("target", "local");
         properties.load (new FileReader (new File (String.format ("src/test/resources/%s.properties", target))));
         dbHelper = new DbHelper ();
-       if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.IE)) {
-            wd = new InternetExplorerDriver();
+        if("".equals (properties.getProperty ("selenium.server"))) {
+
+            if (browser.equals (BrowserType.CHROME)) {
+                wd = new ChromeDriver ();
+            } else if (browser.equals (BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver ();
+            } else if (browser.equals (BrowserType.IE)) {
+                wd = new InternetExplorerDriver ();
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities ();
+            capabilities.setBrowserName (browser);
+            wd = new RemoteWebDriver (new URL (properties.getProperty ("selenium.server")), capabilities);
         }
         wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
        //ожидание, чтобы проверить, если какие-то элементы на странице появляются позже, потому что долго загружаются
