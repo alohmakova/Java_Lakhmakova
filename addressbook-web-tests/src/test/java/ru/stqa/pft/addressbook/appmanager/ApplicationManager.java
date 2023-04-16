690a909b83f88ae09d4fd7ac4c63b444f6bcdadf
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-04-16 23:23:44 +0200

    Запуск тестов на разных платформах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,6 +2,7 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -51,6 +52,7 @@ public class ApplicationManager {
         } else {
             DesiredCapabilities capabilities = new DesiredCapabilities ();
             capabilities.setBrowserName (browser);
+            capabilities.setPlatform (Platform.fromString (System.getProperty ("platform", "win11")));
             wd = new RemoteWebDriver (new URL (properties.getProperty ("selenium.server")), capabilities);
         }
         wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
