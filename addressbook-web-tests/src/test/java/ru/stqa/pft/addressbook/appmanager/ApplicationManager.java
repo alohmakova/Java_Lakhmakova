726c0fc19d68abb06a44daed167fcff70ffc04a9
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-04-17 20:38:44 +0200

    Добаавлена генерация отчётов при помощи Allure

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,9 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Platform;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
@@ -95,4 +92,8 @@ public class ApplicationManager {
     public DbHelper db() {
         return dbHelper;
     }
+    public byte[] takeScreenshot (){
+       return ((TakesScreenshot)wd).getScreenshotAs (OutputType.BYTES);
+    }
 }
+
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -3,10 +3,8 @@ package ru.stqa.pft.addressbook.tests;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
@@ -20,49 +18,54 @@ import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@Listeners(MyTestListener.class)
 public class TestBase {
     Logger logger = LoggerFactory.getLogger (TestBase.class);
 
     protected static final ApplicationManager app
-            = new ApplicationManager(System.getProperty ("browser", BrowserType.CHROME));
+            = new ApplicationManager (System.getProperty ("browser", BrowserType.CHROME));
 
     @BeforeSuite(alwaysRun = true)
-    public void setUp() throws Exception {
-        app.init();
+    public void setUp(ITestContext context) throws Exception {
+        app.init ();
+        context.setAttribute ("app", app);
     }
 
     @AfterSuite(alwaysRun = true)
     public void tearDown() /*throws Exception*/ {
-        app.stop();
+        app.stop ();
     }
 
     @BeforeMethod
-public void logTestStart(Method m, Object[] p){
+    public void logTestStart(Method m, Object[] p) {
         logger.info ("Start test " + m.getName () + " with parameters " + Arrays.asList (p));
     }
+
     @AfterMethod(alwaysRun = true)
-    public void logTestStop(Method m){
+    public void logTestStop(Method m) {
         logger.info ("Stop test " + m.getName ());
     }
+
     public void verifyGroupListInUI() {
         if (Boolean.getBoolean ("verifyUI")) {
-            Groups dbGroups = app.db().groups ();
+            Groups dbGroups = app.db ().groups ();
             Groups uiGroups = app.group ().all ();
             assertThat (uiGroups, equalTo (dbGroups.stream ()
-                    .map ((g) -> new GroupData ().withId (g.getId()).withName (g.getName ()))
-                    .collect(Collectors.toSet())));
+                    .map ((g) -> new GroupData ().withId (g.getId ()).withName (g.getName ()))
+                    .collect (Collectors.toSet ())));
         }
 
     }
+
     public void verifyContactListInUI() {
         if (Boolean.getBoolean ("verifyUI")) {
             Contacts dbContacts = app.db ().contacts ();
             Contacts uiContacts = app.contact ().all ();
             assertThat (uiContacts, equalTo (dbContacts.stream ()
-                    .map ((c) -> new ContactData ().withId (c.getId()).withFirstName (c.getFirstName ()).withLastName (c.getLastName ())
+                    .map ((c) -> new ContactData ().withId (c.getId ()).withFirstName (c.getFirstName ()).withLastName (c.getLastName ())
                             .withAddress (c.getAddress ()).withTelMobile (c.getTelMobile ()).withTelHome (c.getTelHome ())
                             .withTelWork (c.getTelWork ()).withEmail (c.getEmail ()).withEmail2 (c.getEmail2 ()).withEmail3 (c.getEmail3 ()))
-                    .collect(Collectors.toSet())));
+                    .collect (Collectors.toSet ())));
         }
     }
 }
