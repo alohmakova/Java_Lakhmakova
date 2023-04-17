9b068abd80643767affea2152f8a2c5b06a3f2d9
Anastasiya Lakhmakova <bualevar89@gmail.com>
2023-04-17 03:38:55 +0200

    Изменила название платформы
    так как в Jenkins ошибка org.openqa.selenium.WebDriverException: Unrecognized platform: win11

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -52,7 +52,7 @@ public class ApplicationManager {
         } else {
             DesiredCapabilities capabilities = new DesiredCapabilities ();
             capabilities.setBrowserName (browser);
-            capabilities.setPlatform (Platform.fromString (System.getProperty ("platform", "win11")));
+            capabilities.setPlatform (Platform.fromString (System.getProperty ("platform", "Windows 11")));
             wd = new RemoteWebDriver (new URL (properties.getProperty ("selenium.server")), capabilities);
         }
         wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
